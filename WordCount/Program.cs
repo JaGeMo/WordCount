3033bca80a4e65ab2e6da7b5820c6184c95e5046
Jan Moennig <Jan.Moennig@haufe-lexware.com>
2017-11-23 11:40:06 +0100

    second commit

--- WordCount/Program.cs
+++ WordCount/Program.cs
@@ -8,27 +8,44 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
-            // 1.Versuch
+            // first try
             const string t1 = "To be or not to be, that is the question.";
             Console.WriteLine("first try (fixed string): To be or not to be, that is the question.");
             Console.WriteLine(WordCounting.CountWords(t1));
-
+            // second try
             Console.WriteLine("second try / enter new string: ");
             string t2 = Console.ReadLine();
             Console.WriteLine(WordCounting.CountWords(t2));
-
+            // third try
+            Console.WriteLine("third try / enter new string to split: ");
+            string t3 = Console.ReadLine();
+            string[] resultArray = SplitString.stringSplitted(t3);
+            foreach (string result in resultArray)
+            {
+                Console.WriteLine(result);
+            }
             Console.ReadLine();
-
         }
     }
-
-
+    
     public static class WordCounting
     {
         public static int CountWords(string s)
         {
             MatchCollection collection = Regex.Matches(s, @"[\S]+");
             return collection.Count;
+        }
+
+    }
+    
+    public static class SplitString
+    {
+        private static string[] resultArray;
+
+        public static string[] stringSplitted( string in_String)
+        {
+            string[] resultArray = in_String.Split(' ');
+            return resultArray;
         }
 
     }
