709718cf6e393cc2d9c13bc9a3673632ffd4114f
Jan Moennig <Jan.Moennig@haufe-lexware.com>
2017-11-23 11:49:09 +0100

    fourth commit w/o tests

--- WordCount/Program.cs
+++ WordCount/Program.cs
@@ -8,37 +8,35 @@ namespace WordCount
     {
         static void Main(string[] args)
         {
-            // first try
-            const string t1 = "To be or not to be, that is the question.";
-            Console.WriteLine("first try (fixed string): To be or not to be, that is the question.");
-            Console.WriteLine(DirectWordCounting.CountWords(t1));
-            // second try
-            Console.WriteLine("second try / enter new string: ");
-            string t2 = Console.ReadLine();
-            Console.WriteLine(DirectWordCounting.CountWords(t2));
+            //// first try
+            //const string t1 = "To be or not to be, that is the question.";
+            //Console.WriteLine("first try (fixed string): To be or not to be, that is the question.");
+            //Console.WriteLine(DirectWordCounting.CountWords(t1));
+            //// second try
+            //Console.WriteLine("second try / enter new string: ");
+            //string t2 = Console.ReadLine();
+            //Console.WriteLine(DirectWordCounting.CountWords(t2));
 
 
             // third try
             Console.WriteLine("third try / enter new string to split: ");
             string t3 = Console.ReadLine();
             string[] resultArray = SplitString.stringSplitted(t3);
-            foreach (string result in resultArray)
-            {
-                Console.WriteLine(result);
-            }
+            int count = CountStrings.stringsCount(resultArray);
+            Console.WriteLine("number of words: " + count.ToString());
             Console.ReadLine();
         }
     }
     
-    public static class DirectWordCounting
-    {
-        public static int CountWords(string s)
-        {
-            MatchCollection collection = Regex.Matches(s, @"[\S]+");
-            return collection.Count;
-        }
+    //public static class DirectWordCounting
+    //{
+    //    public static int CountWords(string s)
+    //    {
+    //        MatchCollection collection = Regex.Matches(s, @"[\S]+");
+    //        return collection.Count;
+    //    }
 
-    }
+    //}
     
     public static class SplitString
     {
@@ -50,6 +48,21 @@ namespace WordCount
             return resultArray;
         }
 
+    }
+
+    public static class CountStrings
+    {
+        private static int result = 0;
+
+        public static int stringsCount(string[] in_stringArray)
+        {
+            foreach (string word in in_stringArray)
+            {
+                result++;
+            }
+            
+            return result;
+        }
     }
 
 
