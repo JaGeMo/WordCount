fb5cc781813c8fc7d3b19ea244b1376688a3f39c
Jan Moennig <Jan.Moennig@haufe-lexware.com>
2017-11-23 12:13:24 +0100

    fifth commit including tests

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Haufe Group")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright © Haufe Group 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("423a6f26-1bb6-46bf-a8ad-583615d302ce")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCount;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class TestSplitMethod
+    {
+        [TestMethod]
+        public void TestMethodSplit()
+        {
+            // arrange
+
+            // act
+            string[] testSplit = SplitString.stringSplitted("tests tests");
+            string[] testArray = new string[] { "tests", "tests" };
+            // assert
+            CollectionAssert.AreEqual(testArray,testSplit);
+        }
+    }
+
+    [TestClass]
+    public class TestCountMethod
+    {
+        [TestMethod]
+        public void TestMethodCount()
+        {
+            // arrange
+            string[] testArray = new string[] { "one", "two", "three" };
+            // act
+            int counter = CountStrings.stringsCount(testArray);
+            // assert
+            Assert.AreEqual(counter,3);
+        }
+    }
+
+    [TestClass]
+    public class TestSplitMethodSecond
+    {
+        [TestMethod]
+        public void TestMethodSplitSecond()
+        {
+            // arrange
+            string[] testArray = new string[] { "one", "two", "three" };
+            // act
+            string[] testSplit = SplitString.stringSplitted("one two three");
+            // assert
+            Assert.IsTrue(testArray.SequenceEqual(testSplit));
+        }
+    }
+
+    
+}
